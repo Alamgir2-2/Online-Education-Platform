a57420526acba1342964ee1636136b349d2a9432
Alamgir2-2 <darkthehunter22@gmail.com>
2024-09-15 01:45:31 +0700

    Design modified

--- src/Components/Authentication/Signup/Signup.jsx
+++ src/Components/Authentication/Signup/Signup.jsx
@@ -8,51 +8,51 @@ const SignupPage = () => {
 
     return (
         <div className="flex justify-center mt-36">
-            <div className="w-full max-w-lg p-8 bg-white rounded-lg shadow-lg">
-                <h2 className="text-5xl text-black font-bold mb-8 text-center">Sign Up</h2>
+            <div className="w-full max-w-lg p-8 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-lg p-8 shadow-lg">
+                <h2 className="text-5xl text-white font-bold mb-8 text-center">Sign Up</h2>
                 <form>
                     <div className="mb-6 flex items-center">
-                        <label htmlFor="name" className="block text-gray-700 text-lg font-bold w-1/3">
+                        <label htmlFor="name" className="block text-gray-200 text-lg font-bold w-1/3">
                             Name
                         </label>
                         <input 
                             type="text" 
                             id="name" 
-                            className="w-2/3 px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                            className="w-2/3 px-4 py-3 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg p-8 shadow-lg border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
                             placeholder="Enter your name"
                         />
                     </div>
                     <div className="mb-6 flex items-center">
-                        <label htmlFor="email" className="block text-gray-700 text-lg font-bold w-1/3">
+                        <label htmlFor="email" className="block text-gray-200 text-lg font-bold w-1/3">
                             Email
                         </label>
                         <input 
                             type="email" 
                             id="email" 
-                            className="w-2/3 px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                            className="w-2/3 px-4 py-3 bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg p-8 shadow-lg border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
                             placeholder="Enter your email"
                         />
                     </div>
                     <div className="mb-6 flex items-center">
-                        <label htmlFor="password" className="block text-gray-700 text-lg font-bold w-1/3">
+                        <label htmlFor="password" className="block text-gray-200 text-lg font-bold w-1/3">
                             Password
                         </label>
                         <input 
                             type="password" 
                             id="password" 
-                            className="w-2/3 px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
+                            className="w-2/3 px-4 py-3 border bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-lg p-8 shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500" 
                             placeholder="Enter your password"
                         />
                     </div>
                     <div className="mb-6 flex items-center">
-                        <label htmlFor="role" className="block text-gray-700 text-lg font-bold w-1/3">
+                        <label htmlFor="role" className="block text-gray-200 text-lg font-bold w-1/3">
                             Role
                         </label>
                         <select 
                             id="role" 
                             value={role} 
                             onChange={(e) => setRole(e.target.value)} 
-                            className="w-2/3 px-4 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-700"
+                            className="w-2/3 px-4 py-3 border bg-white bg-opacity-10 backdrop-filter backdrop-blur-lg rounded-lg p-8 shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-700"
                         >
                             <option value="">Select Role</option>
                             <option value="student">Student</option>
@@ -68,9 +68,9 @@ const SignupPage = () => {
                         </button>
                     </div>
                 </form>
-                <p className="text-center text-gray-600 text-lg mt-6">
+                <p className="text-center text-gray-200 text-lg mt-6">
                     Already have an account? 
-                    <Link to="/login" className="text-blue-900 hover:text-blue-700 font-bold"> Log in</Link>
+                    <Link to="/login" className="text-blue-500 hover:text-blue-700 font-bold"> Log in</Link>
                 </p>
             </div>
         </div>
