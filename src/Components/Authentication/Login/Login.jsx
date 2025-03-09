5b95c4f96ddc1c13aea5c1261c5c46dde4cd42e7
Alamgir2-2 <darkthehunter22@gmail.com>
2025-03-09 21:27:56 +0500

    Updated

--- src/Components/Authentication/Login/Login.jsx
+++ src/Components/Authentication/Login/Login.jsx
@@ -156,7 +156,7 @@ const LoginPage = () => {
         setLoading(true);
 
         try {
-            const response = await fetch('http://localhost:5000/api/login', {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/login`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
--- src/Components/Authentication/Signup/Signup.jsx
+++ src/Components/Authentication/Signup/Signup.jsx
@@ -14,7 +14,7 @@ const SignupPage = () => {
         e.preventDefault(); // Prevent form submission
 
         try {
-            const response = await fetch('http://localhost:5000/api/signup', {
+            const response = await fetch(`${import.meta.env.VITE_API_URL}/api/signup`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
